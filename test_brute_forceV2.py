fe2816977181f4dbebd4a86382436a8c970fe34a
ilm23411 <44926730+ilm23411@users.noreply.github.com>
2018-12-23 17:43:12 +0800

    Delete test_brute_forceV2.py

--- test_brute_forceV2.py
+++
@@ -1,9 +0,0 @@
-
-import numpy as np
-import lightgbm as lgb
-
-
-data = np.random.rand(500, 10)  # 500 entities, each contains 10 features
-label = np.random.randint(2, size=500)  # binary target
-train_data = lgb.Dataset(data, label=label)
-
